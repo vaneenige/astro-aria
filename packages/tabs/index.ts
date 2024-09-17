5252eaf1fbbfac398c19958000acdbee397d8460
Colin van Eenige <cvaneenige@gmail.com>
2024-09-17 15:50:18 -0600

    Update @astro-aria/tabs

--- packages/tabs/index.ts
+++ packages/tabs/index.ts
@@ -1,20 +1,11 @@
 import Root from "./Root.astro";
 import Tab from "./Tab.astro";
-import TabList from "./TabList.astro";
 import TabPanel from "./TabPanel.astro";
 
 const Tabs = {
   Root,
   Tab,
-  TabList,
   TabPanel,
 };
 
 export default Tabs;
-
-export function extend(extended: { [x: string]: any }) {
-  return {
-    ...Tabs,
-    ...extended,
-  };
-}
